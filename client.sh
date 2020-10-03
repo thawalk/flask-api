7e877c5df82a5df854c92ce81660680374a755e8
Tharun Adhi <tharun@resynctech.com>
2020-10-03 21:24:05 +0800

    init commit

---
+++ client.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+#register a new user
+#POST USER (register)
+echo "---------------------registering a new user-----------------"
+curl --location --request POST 'http://127.0.0.1:5000/register' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+    "username": "tharun",
+    "password": "networks"
+}' \
+
+
+#make the user login
+#POST USER (login)
+echo "---------------------logging in the user-----------------"
+curl --location --request POST 'http://127.0.0.1:5000/auth' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+    "username": "tharun",
+    "password": "networks"
+}'
+
+
+# result=$(curl --location --request POST 'http://127.0.0.1:5000/auth' --header 'Content-Type: application/json' --data-raw '{"username": "tharun","password": "networks"}')
+
+# #myvar1=$( echo $result | jq --raw-output '.access_token' )
+# echo “$result”
+
+#make the store
+#POST STORE
+echo "-----------------------------creating a new store---------------------------"
+curl --location --request POST 'http://127.0.0.1:5000/store/store1' \
+--header 'Content-Type: application/json' \
+--data-raw ''
+
+#post a item (table) to the store, with its price
+#this API call can take 2 mime-types, json and string
+#POST ITEM
+echo "---------------------post a new table details to the store-----------------"
+curl --location --request POST 'http://127.0.0.1:5000/item/table' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+    "price": 15.99,
+    "store_id": 1
+}'
+
+curl --location --request POST 'http://127.0.0.1:5000/item/phone' \
+--header 'Content-Type: text/plain' \
+--data-raw '{
+    "price": 15.99,
+    "store_id": 1
+}'
+
+#get a particular item and its details
+GET ITEM
+curl --location --request GET 'http://127.0.0.1:5000/item/table' \
+--header 'Authorization: JWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZGVudGl0eSI6MSwiaWF0IjoxNjAxNzIzMjM0LCJuYmYiOjE2MDE3MjMyMzQsImV4cCI6MTYwMTcyMzUzNH0.slq-cAovvlEhJXtt7JthMyUjw2dLE5YJqXzlgM7UnRs'
+
+#use put to update the table's price
+#PUT ITEM
+echo "----------------------------update the table's price-----------------------------"
+curl --location --request PUT 'http://127.0.0.1:5000/item/table' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+    "price": 17.99,
+    "store_id": 1
+}'
+
+#get the store details, with the updated table price
+#GET STORE
+echo "---------------------get the store details with updated table price-----------------"
+curl --location --request GET 'http://127.0.0.1:5000/store/store1' \
+--header 'Content-Type: application/json' \
+--data-raw ''
+
+#delete the table from the store
+#DELETE ITEM
+echo "---------------------delete the table from the store-----------------"
+curl --location --request DELETE 'http://127.0.0.1:5000/item/table' \
+--header 'Content-Type: application/json' \
+--data-raw ''
+
+#delete the store
+#DELETE STORE
+echo "---------------------delete the store-----------------"
+curl --location --request DELETE 'http://127.0.0.1:5000/store/store1' \
+--header 'Content-Type: application/json' \
+--data-raw ''
+
+
+
+
+
+
